386aeaabea1aff6380c9e9b82bcf414178402184
axelou987 <77838141+axelou987@users.noreply.github.com>
2021-01-23 21:58:52 +0100

    Update dbconnexion.php

--- dbconnexion.php
+++ dbconnexion.php
@@ -1,7 +1,16 @@
 <?php
-$nomServeurbd = "localhost";
-$nomUserbd = "root";
-$mdpbd = "";
-$nombd = "cooks_drawer"
 
-$co = mysqli_connect($nomServeurbd, $nomUserbd, $mdpbd, $nombd);
+class Dbh {
+    private $host = "localhost";
+    private $user = "root";
+    private $pwd = "";
+    private $dbName = "cooks_drawer";
+
+    protected function connect(){
+        $dsn = 'mysql:host=' . $this->host . ';dbname=' . $thsi->dbName;
+        $pdo = new PDO($dsn, $this->user, $this->pwd);
+        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        return $pdo;
+    
+    }
+}
