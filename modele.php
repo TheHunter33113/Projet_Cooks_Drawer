785dfba91ff4deb2c9a636c0ec1e45ec5ad28ad1
axelou987 <77838141+axelou987@users.noreply.github.com>
2021-01-23 23:35:43 +0100

    Update modele.php

--- modele.php
+++ modele.php
@@ -1,8 +1,21 @@
 <?php
 
-class modele {
-	
-	public function __construct(){
+class Objet extends Dbconnexion {
 
-	}
+    protected function getObjet($nomObj){
+        $sql =" SELECT * FROM objet WHERE nomObj = ?";
+        $stmt = $this->connect()->prepare($sql);
+        $stmt->execute([$nomObj]);
+
+        $results = $stmt->fetchAll();
+        return $results;
+    }
+
+    protected function setObjet($nomObjet, $description ,$Prix){
+        $sql = "INSERT TO objet(objet_nomObjet, objet_description ,objet_Prix");
+        $stmt = $this->connect()->prepare($sql);
+        $stmt->execute([($nomObjet, $description ,$Prix]);
+
+    }
+    
 }
