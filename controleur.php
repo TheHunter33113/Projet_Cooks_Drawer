96b00bd31ec59831ba17732e2eab025e2c14b116
axelou987 <77838141+axelou987@users.noreply.github.com>
2021-01-23 23:04:37 +0100

    Update controleur.php

--- controleur.php
+++ controleur.php
@@ -1,8 +1,24 @@
-<?php
-
-class controleur {
-	
-	public function __construct(){
-
-	}
-}
+class controleur extends model {  
+     public $model;   
+  
+     public function __construct()    
+     {    
+          $this->model = new Model();  
+     }   
+      
+     public function invoke()  
+     {  
+          if (!isset($_GET['objet']))  
+          {  
+               // no special book is requested, we'll show a list of all available books  
+               $books = $this->model->getobjetlist();  
+               include 'view/objetlist.php'; 
+          } 
+          else 
+          { 
+               // show the requested Objet 
+               $book = $this->model->getobjet($_GET['objet']); 
+               include 'view/objet.php';  
+          }  
+     }  
+}  
