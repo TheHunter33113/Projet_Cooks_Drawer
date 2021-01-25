873352746c1c166fe83942bd566908623db3df2e
nitharsan95 <64790631+nitharsan95@users.noreply.github.com>
2021-01-25 08:49:40 +0100

    Update Login.php

--- Login.php
+++ Login.php
@@ -8,7 +8,7 @@
 <head>
 	<meta charset="utf_8">
 	<title>Login form</title>
-	<link rel="stylesheet" type="text/css" href="css/style1.css">
+	<link rel="stylesheet" type="text/css" href="style1.css">
 </head>
 <body>
 	<div id="container">
