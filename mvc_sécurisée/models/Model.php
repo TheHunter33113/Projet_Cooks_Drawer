b6e50ecbbee7b411dd429e4313ecc96423927823
TheHunter33113 <64205606+TheHunter33113@users.noreply.github.com>
2021-01-25 07:30:15 +0000

    Update Model.php

--- mvc_sécurisée/models/Model.php
+++ mvc_sécurisée/models/Model.php
@@ -3,27 +3,33 @@
 abstract class Model
 {
     //instancie la connexion a la bdd
-    private static $dbconnexion
+    private static $_bdd;
 
     private static function setBdd()
     {
-        self::$bd = new PDO('mysql:host=localhost;dbname=cokks_drawer;charset=utf8',
-        'root', '');
-        self::$_bdd->setAtrribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+        $options = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ];
+        self::$_bdd = new PDO('mysql:host=localhost;dbname=cooks_drawer;charset=utf8',
+        'root', '', $options);
+       // self::$_bdd->setAtrribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 
     }
 
     //recuperer la connexion a la bdd
     protected function getBdd()
     {
-        self::setBdd();
-        return self::$_bdd
+        if (self::$_bdd == null) {
+                self::setBdd();
+                return self::$_bdd;
+        }
+ 
     }
 
     protected function getAll($table, $obj)
     {
         $var = [];
-        $req = $this->getBdd()->prepare('SELECT * FROM' .table. 'ORDER BY id desc');
+        $req = $this->getBdd()->prepare('SELECT * FROM' .$table. 'ORDER BY id desc');
         $req->execute();
         while($data = $req->fetch(PDO::FETCH_ASSOC))
         {
